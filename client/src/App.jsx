809b54c9a5a1d79c89502b485a535731364c4b13
Loren Q <lorenquiceno@gmail.com>
2023-05-30 20:09:25 -0500

    Added Tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
-import "./App.css";
-import { FiMail } from "react-icons/fi";
-import { FiEye } from "react-icons/fi";
+// import { useState } from "react";
+import './App.css'
+import { FiMail } from 'react-icons/fi'
+import { FiEye } from 'react-icons/fi'
 
 function App() {
   return (
     <>
-      <main>
+      <main className="grid grid-cols-2">
         <section className="sideLogin">
           <header>
-            <h1>SENA</h1>
+            <h1 className="text-red-700">SENA</h1>
           </header>
           <div className="formLogin">
             <h2>BIENVENIDO DE VUELTA</h2>
-            <span className="descripcionLogin">
-              Es un placer tenerte de nuevo aquí
-            </span>
+            <span className="descripcionLogin">Es un placer tenerte de nuevo aquí</span>
             <form action="">
               <div className="">
                 <FiMail className="icon" />
@@ -29,21 +27,14 @@ function App() {
             </form>
           </div>
           <footer>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-              accusamus quisquam recusandae in necessitatibus nobis omnis cum
-              quo debitis voluptatum beatae deleniti. Beatae eaque commodi dolor
-              accusantium nam quibusdam sunt.
-            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, accusamus quisquam recusandae in necessitatibus nobis omnis cum quo debitis voluptatum beatae deleniti. Beatae eaque commodi dolor accusantium nam quibusdam sunt.</p>
           </footer>
         </section>
 
-        <section className="sectionVerde">
-          
-        </section>
+        <section className="sectionVerde"></section>
       </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
