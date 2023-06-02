2428157ec0cff340aa793b05b63a921194bba5d5
Stiven Blandón <blandon0207s@gmail.com>
2023-06-01 23:34:49 -0500

    sql script fixed

--- server/src/controllers/inscriptions.controllers.ts
+++ server/src/controllers/inscriptions.controllers.ts
@@ -7,7 +7,7 @@ import { type id } from '../models/user.interfaces.js'
 
 export const getInscriptions = async (_req: Request<inscription>, res: Response): Promise<Response> => {
   try {
-    const [inscriptions] = await connection.query('SELECT * FROM modalidades_etapa_practica')
+    const [inscriptions] = await connection.query('SELECT * FROM inscripciones')
     return res.status(httpStatus.OK).json({ data: inscriptions })
   } catch (error) {
     return handleHTTP(res, 'ERROR_GET_PRACTICAL_STAGES')
@@ -17,7 +17,7 @@ export const getInscriptions = async (_req: Request<inscription>, res: Response)
 export const getInscriptionById = async ({ params }: Request<id>, res: Response): Promise<Response> => {
   const { id } = params
   try {
-    const [inscription] = await connection.query('SELECT * FROM modalidades_etapa_practica WHERE id_modalidad_etapa_practica = ?', [id])
+    const [inscription] = await connection.query('SELECT * FROM inscripciones WHERE id_inscripcion = ?', [id])
     return res.status(httpStatus.OK).json({ data: inscription })
   } catch (error) {
     return handleHTTP(res, 'ERROR_GET_PRACTICAL_STAGE')
@@ -31,7 +31,7 @@ export const createInscription = async ({ body }: Request<inscription>, res: Res
       'INSERT INTO inscripciones (nombres_aprendiz_inscripcion, apellidos_aprendiz_inscripcion, tipo_documento_aprendiz_inscripcion, numero_documento_aprendiz_inscripcion, correo_electronico_aprendiz_inscripcion, numero_telefono_aprendiz_inscripcion, numero_ficha_aprendiz_inscripcion, programa_formacion_aprendiz_inscripcion, inicio_etapa_practica_aprendiz_inscripcion, fin_etapa_practica_aprendiz_inscripcion, fecha_creacion_aprendiz_inscripcion) VALUE (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURDATE())',
       [nombres_aprendiz_inscripcion, apellidos_aprendiz_inscripcion, tipo_documento_aprendiz_inscripcion, numero_documento_aprendiz_inscripcion, correo_electronico_aprendiz_inscripcion, numero_telefono_aprendiz_inscripcion, numero_ficha_aprendiz_inscripcion, programa_formacion_aprendiz_inscripcion, inicio_etapa_practica_aprendiz_inscripcion, fin_etapa_practica_aprendiz_inscripcion]
     )
-    return res.status(httpStatus.OK).json(true)
+    return res.status(httpStatus.OK).json({ data: 'Done' })
   } catch (error) {
     return handleHTTP(res, 'ERROR_CREATE_INSCRIPTION')
   }
