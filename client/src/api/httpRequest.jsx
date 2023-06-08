86807b2d2ca2fb7a5be1f79f1debafab5373ea5f
Cristian Garcia <cristiangt1015@gmail.com>
2023-06-08 11:23:38 -0500

    get apprentices, tarinees view

---
+++ client/src/api/httpRequest.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+
+// Obtenemos todos los aprendices
+export const GetUsersHttp = async () => {
+  // pasamos la url que será la ruta de la api
+  URL = "http://localhost:3000/api/inscriptions";
+
+  // hacemos la petición get
+  const response = await axios.get(URL);
+  return response;
+};
