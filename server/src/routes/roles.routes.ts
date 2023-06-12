567c00894c65734e73aa18f3a9ae3c536b3d3355
ConanGH-S <blandon0207s@gmail.com>
2023-06-11 19:20:44 -0500

    added roles routes

--- server/src/routes/roles.routes.ts
+++ server/src/routes/roles.routes.ts
@@ -1,11 +1,15 @@
 import { type IRouter, Router } from 'express'
-import { createRole } from '../controllers/roles.controllers.js'
+import { createRole, editRole, getRoles } from '../controllers/roles.controllers.js'
 
 const roleRoutes: IRouter = Router()
 
 // * GET
+roleRoutes.get('/roles', getRoles)
 
 // * POST
 roleRoutes.post('/create-role', createRole)
+
+// * PATCH
+roleRoutes.patch('/edit-role/:id', editRole)
 
 export { roleRoutes }
