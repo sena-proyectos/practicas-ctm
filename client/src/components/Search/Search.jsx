36b5067774fbed6db42ad88a2066fc4784fe4354
Loren Q <lorenquiceno@gmail.com>
2023-06-16 13:29:55 -0500

    component search was created

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,3 +1,16 @@
-const Search = () => {}
+import { IoSearchOutline } from 'react-icons/io5'
+
+import { Button } from '../Button/Button'
+
+const Search = () => {
+  return (
+    <article className="w-1/3 h-9 flex items-center justify-center ">
+      <form action="" method="get" className="w-full saturate-[100%] flex items-center bg-gray rounded-3xl backdrop-blur-sm ">
+        <input type="text" placeholder="Search" className="w-full px-5 py-1 flex-1 rounded-3xl outline-none bg-transparent" />
+        <Button bg={'bg-transparent'} px="px-3" textColor="text-black" className="absolute right-4" value={<IoSearchOutline />} />
+      </form>
+    </article>
+  )
+}
 
 export { Search }
