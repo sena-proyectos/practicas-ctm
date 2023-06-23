abd9ba069c62279c51ddb2b066f0a5c8b26911e9
ConanGH-S <blandon0207s@gmail.com>
2023-06-22 19:12:59 -0500

    get students routes added

--- server/src/controllers/users.controllers.ts
+++ server/src/controllers/users.controllers.ts
@@ -52,7 +52,7 @@ export const getTeachers = async (req: Request, res: Response): Promise<Response
     const offset = (Number(page) - 1) * Number(limit)
 
     const [teachers] = await connection.query('SELECT * FROM usuarios WHERE id_rol = 2 LIMIT ? OFFSET ?', [limit, offset])
-    if (!Array.isArray(teachers) || teachers.length === 0) throw new DbErrorNotFound('No hay estudiantes registrados.', errorCodes.ERROR_GET_STUDENTS)
+    if (!Array.isArray(teachers) || teachers.length === 0) throw new DbErrorNotFound('No hay instructores registrados.', errorCodes.ERROR_GET_TEACHER)
 
     const [total] = await connection.query('SELECT COUNT(*) as count FROM usuarios WHERE id_rol = 3') as unknown as Array<{ count: number }>
     const totalPages = Math.ceil(Number((Array.isArray(total) && total[0].count)) / Number(limit))
@@ -97,9 +97,9 @@ export const getStudentsById: RequestHandler<{ id: string }, Response, LoginData
   const { id } = req.params
   const idNumber = Number(id)
   try {
-    const [teacher] = await connection.query('SELECT * FROM usuarios WHERE id_rol = 3 AND id_usuario = ?', [idNumber])
-    if (!Array.isArray(teacher) || teacher.length === 0) throw new DbErrorNotFound('No se encontró el estudiante.', errorCodes.ERROR_GET_TEACHER)
-    return res.status(httpStatus.OK).json({ data: teacher })
+    const [student] = await connection.query('SELECT * FROM usuarios WHERE id_rol = 3 AND id_usuario = ?', [idNumber])
+    if (!Array.isArray(student) || student.length === 0) throw new DbErrorNotFound('No se encontró el estudiante.', errorCodes.ERROR_GET_STUDENT)
+    return res.status(httpStatus.OK).json({ data: student })
   } catch (error) {
     return handleHTTP(res, error as CustomError)
   }
