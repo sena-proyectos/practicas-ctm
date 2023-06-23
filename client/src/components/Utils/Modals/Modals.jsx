e3b293b789ba07c71b0e84bdbf078f39a38219fc
Loren Q <lorenquiceno@gmail.com>
2023-06-23 12:10:03 -0500

    info modal student uncompleted

--- client/src/components/Utils/Modals/Modals.jsx
+++ client/src/components/Utils/Modals/Modals.jsx
@@ -1,7 +1,7 @@
 const Modals = () => {
   return (
-    <section className="w-full min-h-full fixed top-0 left-0 bg-black/[0.5] items-center justify-center flex">
-      <section className="w-1/2 bg-emerald-300 relative rounded-md shadow-md h-[30rem]">
+    <section className="w-screen h-screen fixed top-0 left-0 bg-black/10 items-center justify-center flex z-50">
+      <section className="w-1/2 bg-white relative rounded-md shadow-md h-[30rem]">
         <header>
           <h1 className="text-2xl text-center">Modal</h1>
         </header>
