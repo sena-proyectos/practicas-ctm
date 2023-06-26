34cde6c79b98fa812d8bb303b7147f0f56c3aea2
Stiven Benjumea <stevenbenjumea9@gmail.com>
2023-06-26 13:23:24 -0500

    dberror add

--- server/src/controllers/inscriptions.controllers.ts
+++ server/src/controllers/inscriptions.controllers.ts
@@ -3,7 +3,7 @@ import { type inscriptionData } from '../interfaces/inscriptions.interfaces.js'
 import { connection } from '../config/db.js'
 import { httpStatus } from '../models/httpStatus.enums.js'
 import { handleHTTP } from '../errors/errorsHandler.js'
-import { type CustomError } from '../errors/customErrors.js'
+import { DbError, type CustomError } from '../errors/customErrors.js'
 
 export const getInscriptions = async (_req: Request, res: Response): Promise<Response> => {
   try {
@@ -45,9 +45,9 @@ export const createInscription: RequestHandler<{}, Response, inscriptionData> = 
     telefono_jefe_inmediato_inscripcion,
     correo_jefe_inmediato_inscripcion,
     asume_pago_arl_inscripcion,
-    link_documentos_pdf_inscripcion,
+    // link_documentos_pdf_inscripcion,
     observaciones_inscripcion,
-    fecha_creacion_inscripcion,
+    // fecha_creacion_inscripcion,
     id_usuario_responsable_inscripcion
   } = req.body
   try {
@@ -77,6 +77,41 @@ export const createInscription: RequestHandler<{}, Response, inscriptionData> = 
       ]
     )
     return res.status(httpStatus.CREATED).json({ message: 'Inscripción creada.' })
+  } catch (error) {
+    return handleHTTP(res, error as CustomError)
+  }
+}
+
+export const editInscription: RequestHandler<{ id: string }, Response, inscriptionData> = async (req: Request<{ id: string }>, res: Response): Promise<Response> => {
+  const {
+    id_modalidad_inscripcion,
+    nombres_inscripcion,
+    apellidos_inscripcion,
+    tipo_documento_inscripcion,
+    numero_documento_inscripcion,
+    correo_electronico_inscripcion,
+    numero_celular_inscripcion,
+    etapa_formacion_actual_inscripcion,
+    nivel_formacion_actual_inscripcion,
+    id_ficha_inscripcion,
+    id_instructor_lider_inscripcion,
+    apoyo_sostenimiento_inscripcion,
+    id_empresa_inscripcion,
+    nombre_completo_jefe_inmediato_inscripcion,
+    cargo_jefe_inmediato_inscripcion,
+    telefono_jefe_inmediato_inscripcion,
+    correo_jefe_inmediato_inscripcion,
+    asume_pago_arl_inscripcion,
+    link_documentos_pdf_inscripcion,
+    observaciones_inscripcion,
+    fecha_creacion_inscripcion,
+    id_usuario_responsable_inscripcion
+  } = req.body
+  const { id } = req.params
+  try {
+    const [inscription] = await connection.query('UPDATE inscripciones SET id_modalidad_inscripcion = IFNULL(?, id_modalidad_inscripcion), nombres_inscripcion = IFNULL(?,nombres_inscripcion), apellidos_inscripcion = IFNULL(?, apellidos_inscripcion), tipo_documento_inscripcion = IFNULL(?, tipo_documento_inscripcion), numero_documento_inscripcion = IFNULL(?, numero_documento_inscripcion), correo_electronico_inscripcion = IFNULL(?, correo_electronico_inscripcion), numero_celular_inscripcion = IFNULL(?, numero_celular_inscripcion), etapa_formacion_actual_inscripcion = IFNULL(?, etapa_formacion_actual_inscripcion), nivel_formacion_actual_inscripcion = IFNULL(?, nivel_formacion_actual_inscripcion), id_ficha_inscripcion = IFNULL(?, id_ficha_inscripcion), id_instructor_lider_inscripcion = IFNULL(?, id_instructor_lider_inscripcion), apoyo_sostenimiento_inscripcion = IFNULL(?, apoyo_sostenimiento_inscripcion), id_empresa_inscripcion = IFNULL(?, id_empresa_inscripcion), nombre_completo_jefe_inmediato_inscripcion = IFNULL(?, nombre_completo_jefe_inmediato_inscripcion), cargo_jefe_inmediato_inscripcion = IFNULL(?, cargo_jefe_inmediato_inscripcion), telefono_jefe_inmediato_inscripcion = IFNULL(?, telefono_jefe_inmediato_inscripcion), correo_jefe_inmediato_inscripcion = IFNULL(?, correo_jefe_inmediato_inscripcion), asume_pago_arl_inscripcion = IFNULL(?, asume_pago_arl_inscripcion), link_documentos_pdf_inscripcion = IFNULL(?, link_documentos_pdf_inscripcion), observaciones_inscripcion = IFNULL(?, observaciones_inscripcion), fecha_creacion_inscripcion = IFNULL(?, fecha_creacion_inscripcion), id_usuario_responsable_inscripcion = IFNULL(?, id_usuario_responsable_inscripcion) WHERE id_inscripcion = ? ', [id_modalidad_inscripcion, nombres_inscripcion, apellidos_inscripcion, tipo_documento_inscripcion, numero_documento_inscripcion, correo_electronico_inscripcion, numero_celular_inscripcion, etapa_formacion_actual_inscripcion, nivel_formacion_actual_inscripcion, id_ficha_inscripcion, id_instructor_lider_inscripcion, apoyo_sostenimiento_inscripcion, id_empresa_inscripcion, nombre_completo_jefe_inmediato_inscripcion, cargo_jefe_inmediato_inscripcion, telefono_jefe_inmediato_inscripcion, correo_jefe_inmediato_inscripcion, asume_pago_arl_inscripcion, link_documentos_pdf_inscripcion, observaciones_inscripcion, fecha_creacion_inscripcion, id_usuario_responsable_inscripcion, id])
+    if (!Array.isArray(inscription) && inscription?.affectedRows === 0) throw new DbError('No se pudo modificar la inscripción.')
+    return res.status(httpStatus.OK).json({ message: 'Inscripción modificada con éxito.' })
   } catch (error) {
     return handleHTTP(res, error as CustomError)
   }
