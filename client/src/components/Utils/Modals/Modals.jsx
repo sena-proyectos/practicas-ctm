631bec7c7a17d2984bf11f051eff8b74eb3aee67
Loren Q <lorenquiceno@gmail.com>
2023-06-27 09:13:00 -0500

    modal student info structure -style

--- client/src/components/Utils/Modals/Modals.jsx
+++ client/src/components/Utils/Modals/Modals.jsx
@@ -1,12 +1,95 @@
-const Modals = () => {
+import { IoMdClose } from 'react-icons/io'
+import { Button } from '../../Button/Button'
+
+const Modals = ({ closeModal, title, bodyStudent = false, emailStudent, documentStudent, celStudent, trainingProgram, ficha, academicLevel, trainingStage, modalitie, finLectiva, inicioProductiva, company, innmediateSuperior, emailSuperior, workstation, celSuperior, arl, bodyFilter = false }) => {
+  const handleModal = () => {
+    closeModal()
+  }
   return (
-    <section className="w-screen h-screen fixed top-0 left-0 bg-black/10 items-center justify-center flex z-50">
-      <section className="w-1/2 bg-white relative rounded-md shadow-md h-[30rem]">
-        <header>
-          <h1 className="text-2xl text-center">Modal</h1>
+    <section className=" w-screen h-screen fixed top-0 left-0 bg-black/70 items-center justify-center flex z-50">
+      <section className="w-1/2 bg-white relative rounded-2xl shadow-md h-auto flex flex-col">
+        <IoMdClose className="w-7 h-7 absolute top-[6px] right-5 cursor-pointer" onClick={handleModal} />
+        <header className="grid place-items-center pt-2">
+          <h2 className="text-xl w-fit text-center font-medium border-b-1 border-primary">{title}</h2>
         </header>
-        <body></body>
-        <footer></footer>
+        <section className="flex-auto w-5/6 mx-auto">
+          {bodyStudent && (
+            <>
+              <section className="pt-3 pb-2">
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Email:</span>
+                  <span>{emailStudent}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Documento de identidad:</span>
+                  <span>{documentStudent}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Contacto:</span>
+                  <span>{celStudent}</span>
+                </section>
+              </section>
+              <hr className="w-full text-primary" />
+              <section className="py-2">
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Programa:</span>
+                  <span>{trainingProgram}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Ficha:</span>
+                  <span>{ficha}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Nivel académico:</span>
+                  <span>{academicLevel}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Etapa:</span>
+                  <span>{trainingStage}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Modalidad:</span>
+                  <span>{modalitie}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Fin lectiva:</span>
+                  <span>{finLectiva}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Inicio productiva:</span>
+                  <span>{inicioProductiva}</span>
+                </section>
+              </section>
+              <hr className="w-full text-primary" />
+              <section className="pt-2 py-3">
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Empresa:</span>
+                  <span>{company}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Jefe Inmediato:</span>
+                  <span>{innmediateSuperior}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Cargo:</span>
+                  <span>{workstation}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Email:</span>
+                  <span>{emailSuperior}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">Contacto:</span>
+                  <span>{celSuperior}</span>
+                </section>
+                <section className="grid grid-cols-2-45-55">
+                  <span className="font-medium">ARL:</span>
+                  <span>{arl}</span>
+                </section>
+              </section>
+            </>
+          )}
+        </section>
       </section>
     </section>
   )
