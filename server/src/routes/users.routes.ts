6991757b4030e6872ea9d8cbf3ed8e8b3d878dfa
ConanGH-S <blandon0207s@gmail.com>
2023-06-28 08:16:03 -0500

    get user by name change to req.query

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -10,10 +10,10 @@ userRoutes.get('/users', getUsers)
 userRoutes.get('/user/:id', checkIdReq, getUserById)
 userRoutes.get('/teachers', getTeachers)
 userRoutes.get('/teacher/:id', checkIdReq, getTeachersById)
-userRoutes.get('/teacherName', checkName, getTeacherByName)
 userRoutes.get('/students', getStudents)
 userRoutes.get('/student/:id', checkIdReq, getStudentsById)
 userRoutes.get('/studentName', checkName, getStudentByName)
+userRoutes.get('/teacherName', checkName, getTeacherByName)
 
 // * POST
 userRoutes.post('/register', checkRegisterData, checkExistingUser, createUser)
