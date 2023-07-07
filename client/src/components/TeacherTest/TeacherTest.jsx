81f030cde625cd56b59785dd5cc45b35ab163e1e
STBenji <stevenbenjumea9@gmail.com>
2023-07-07 11:20:01 -0500

    teacher test deleted

--- client/src/components/TeacherTest/TeacherTest.jsx
+++ client/src/components/TeacherTest/TeacherTest.jsx
@@ -1,13 +1,37 @@
-
+import { useRef } from 'react'
+import { GetClassByNumber } from '../../api/httpRequest'
+import Swal from 'sweetalert2'
 
 export const TeacherName = () => {
-  
+    const ficha = useRef()
 
-  return (
-    <main>
-      <h1>hola</h1>
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const numFicha = ficha.current.value
 
+        try {
+            const res = await GetClassByNumber(numFicha)
+            const response = res.data.data[0]
+            console.log(response)
+        } catch (error) {
+            const message = error.response.data.error.info.message
+            Swal.fire({
+                icon: 'error',
+                title: 'Opsss!!',
+                text: message,
+            })
+        }
+    }
 
-    </main>
-  )
+    return (
+        <main>
+            <h1>numero por ficha</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label>Ingrese el número de la ficha:</label>
+                <br />
+                <input type="text" ref={ficha} className="border-2" />
+            </form>
+        </main>
+    )
 }
