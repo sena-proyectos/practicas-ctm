fb6527013f0b7cdff3ed31f683e69fc79677608e
Loren Q <lorenquiceno@gmail.com>
2023-07-27 12:56:55 -0500

    feat: route protection by id

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,20 +11,33 @@ import { Settings } from './components/Settings/Settings'
 import { Teachers } from './components/Teachers/Teachers'
 import { AssignClass } from './components/Assign-class/AssignClass'
 import { Approvement } from './components/Approvement/approvement'
+import { useSelector } from 'react-redux'
+import { ProtectedRoute } from './ProtectedRoute'
+import { keysRoles } from './import/staticData'
 
 const App = () => {
+  const idRol = useSelector((state) => state.id_rol)
+
   return (
     <Routes>
       <Route path="/" element={<User />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/inscribir-aprendiz" element={<RegisterStudent />} />
-      <Route path="/aprendices" element={<Student />} />
-      <Route path="/visitas" element={<Visits />} />
-      <Route path="/bitacoras" element={<Bitacoras />} />
-      <Route path="/config" element={<Settings />} />
-      <Route path="/instructores" element={<Teachers />} />
-      <Route path="/asignar-ficha" element={<AssignClass />} />
-      <Route path="/aprov" element={<Approvement />} />
+      <Route element={<ProtectedRoute idRol={idRol} />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/config" element={<Settings />} />
+        <Route path="/aprendices" element={<Student />} />
+        <Route path="/aprov" element={<Approvement />} />
+        <Route path="/asignar-ficha" element={<AssignClass />} />
+      </Route>
+
+      <Route element={<ProtectedRoute idRol={idRol === Number(keysRoles[0]) || idRol === Number(keysRoles[1])} redirectTo="/home" />}>
+        <Route path="inscribir-aprendiz" element={<RegisterStudent />} />
+        <Route path="/instructores" element={<Teachers />} />
+      </Route>
+
+      <Route element={<ProtectedRoute idRol={idRol === Number(keysRoles[0]) || idRol === Number(keysRoles[2])} redirectTo="/home" />}>
+        <Route path="/visitas" element={<Visits />} />
+        <Route path="/bitacoras" element={<Bitacoras />} />
+      </Route>
     </Routes>
   )
 }
