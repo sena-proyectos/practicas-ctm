1d42d6b8e6dc317d173d61a4eae4fa040022c922
Loren Q <lorenquiceno@gmail.com>
2023-08-01 12:05:23 -0500

    style: media queries cards

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
         <header className="grid place-items-center">
           <h1 className="text-2xl font-medium text-center">Bienvenido </h1>
         </header>
-        <div className="grid grid-cols-1 gap-1 p-5 sm:grid-cols-2 md:grid-cols-3 ">
-          {rolesCard[idRol ?? 1].map(({ title, titleColor, description, buttonText, bgColor, sombra, link }) => {
-            return <Card cardHome bgColor={bgColor} shadow={`shadow-inner-custom shadow-${sombra}`} scale={'scale-90'} titleColor={titleColor} title={title} description={description} roundedLink={'rounded-md'} buttonText={buttonText} key={title} link={link} transition={`transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:${bgColor}/100 duration-500`} />
+        <div className="grid grid-cols-1 gap-1 p-4 sm:grid-cols-2 md:grid-cols-3">
+          {rolesCard[idRol].map(({ title, titleColor, description, buttonText, bgColor, sombra, link }) => {
+            return <Card cardHome height={'h-[11.5rem] sm:h-[15.7rem] md:h-[14rem]'} bgColor={bgColor} shadow={`shadow-inner-custom shadow-${sombra}`} scale={'scale-90'} titleColor={titleColor} title={title} description={description} roundedLink={'rounded-md'} buttonText={buttonText} key={title} link={link} transition={`transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:${bgColor}/100 duration-500`} />
           })}
         </div>
 
