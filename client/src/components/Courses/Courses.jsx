0c86e4618e5bf5627df7891ddc9e950ee631c0fd
Loren Q <lorenquiceno@gmail.com>
2023-08-08 11:49:35 -0500

    feat: fichas pagination and cards

--- client/src/components/Courses/Courses.jsx
+++ client/src/components/Courses/Courses.jsx
@@ -1,8 +1,78 @@
+import { useState } from 'react'
+
+//Componentes
 import { Footer } from '../Footer/Footer'
 import { Search } from '../Search/Search'
 import { Siderbar } from '../Siderbar/Sidebar'
+import { Card3D } from '../Utils/Card/Card'
+import { Pagination } from '../Utils/Pagination/Pagination'
 
 export const Courses = () => {
+  const [pageNumber, setPageNumber] = useState(0)
+  const Courses = {
+    data: [
+      {
+        ficha: 2473196,
+        name: 'Fabricación de muebles contemporaneos',
+        lider: 'Peranito 1',
+        seguimiento: 'Seguimient 1',
+        etapa: 'Lectiva',
+        finLectiva: '04-05-23',
+        inicioProductiva: '04-06-23',
+      },
+      {
+        ficha: 2689476,
+        name: 'Analisis y desarrollo de software',
+        lider: 'Peranito 2',
+        seguimiento: 'Seguimient 2',
+        etapa: 'Productiva',
+        finLectiva: '04-05-23',
+        inicioProductiva: '04-06-23',
+      },
+      {
+        ficha: 2869467,
+        name: 'Pan y Tomate',
+        lider: 'Peranito 3',
+        seguimiento: 'Seguimient 3',
+        etapa: 'Lectiva',
+        finLectiva: '04-05-23',
+        inicioProductiva: '04-06-23',
+      },
+      {
+        ficha: 1234567,
+        name: 'Fabricación de muebles contemporaneos',
+        lider: 'Peranito 4',
+        seguimiento: 'Seguimient 4',
+        etapa: 'Productiva',
+        finLectiva: '04-05-23',
+        inicioProductiva: '04-06-23',
+      },
+      {
+        ficha: 7654321,
+        name: 'Analisis y desarrollo de software',
+        lider: 'Peranito 5',
+        seguimiento: 'Seguimient 5',
+        etapa: 'Lectiva',
+        finLectiva: '04-05-23',
+        inicioProductiva: '04-06-23',
+      },
+      {
+        ficha: 1234765,
+        name: 'Pan y Tomate',
+        lider: 'Peranito 6',
+        seguimiento: 'Seguimient 6',
+        etapa: 'Productiva',
+        finLectiva: '04-05-23',
+        inicioProductiva: '04-06-23',
+      },
+    ],
+  }
+
+  const coursesPerPage = 6
+  const pageCount = Math.ceil(Courses.data.length / coursesPerPage)
+  const startIndex = pageNumber * coursesPerPage
+  const endIndex = startIndex + coursesPerPage
+
   return (
     <main className="flex flex-row min-h-screen">
       <Siderbar />
@@ -10,12 +80,14 @@ export const Courses = () => {
         <header className="grid place-items-center">
           <Search searchFilter />
         </header>
-        <section className="grid grid-cols-1 px-10 pt-5 pb-2 gap-x-4 gap-y-3 sm:grid-cols-2 md:grid-cols-3">
-          <div className="[perspective:1000px] group flex flex-col gap-1 rounded-xl md:h-[10.5rem] sm:h-[10rem] ">
-            <div className="relative w-full h-full rounded-xl shadow-xl transition-all duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-              <div className="absolute w-full h-full bg-blue-300 rounded-xl"></div>
-              <div className="absolute w-full h-full rounded-xl bg-slate-500 [transform:rotateY(180deg)] [backface-visibility:hidden]"></div>
-            </div>
+        <section>
+          <section className="grid grid-cols-1 px-10 pt-5 pb-2 gap-x-4 gap-y-3 sm:grid-cols-2 md:grid-cols-3">
+            {Courses.data.slice(startIndex, endIndex).map((course, i) => {
+              return <Card3D key={i} header={course.ficha} title={course.name} subtitle={course.etapa} item1={course.seguimiento} item2={course.lider} item3={course.finLectiva} item4={course.inicioProductiva} />
+            })}
+          </section>
+          <div className="flex justify-center h-[13vh] relative bottom-0">
+            <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} pageCount={pageCount} />
           </div>
         </section>
         <Footer />
