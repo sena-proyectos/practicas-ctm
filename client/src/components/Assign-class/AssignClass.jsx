2e97e60b07aec8116be448e0e3cdc789c5f5c975
Loren Q <lorenquiceno@gmail.com>
2023-08-11 13:01:54 -0500

    style: card design changed

--- client/src/components/Assign-class/AssignClass.jsx
+++ client/src/components/Assign-class/AssignClass.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
 //icons
-import { FaUser } from 'react-icons/fa'
 import { HiOutlineUserAdd } from 'react-icons/hi'
+import { BsJournalBookmark } from 'react-icons/bs'
 
 //Componentes
 import { Siderbar } from '../Siderbar/Sidebar'
@@ -43,21 +43,6 @@ export const AssignClass = () => {
     ],
   }
 
-  const colorsCard = [
-    {
-      bordercolor: 'border-sky-400',
-      bgcolor: 'bg-third',
-    },
-    {
-      bordercolor: 'border-sky-950',
-      bgcolor: 'bg-slate-500',
-    },
-  ]
-
-  const allColors = courses.data.map((_, index) => ({
-    ...colorsCard[index % colorsCard.length],
-  }))
-
   const handleAsign = () => {
     setModalAsign(!modalAsign)
   }
@@ -73,24 +58,27 @@ export const AssignClass = () => {
           </header>
           <section>
             <section className="grid grid-cols-1 px-10 pt-5 pb-2 gap-x-4 gap-y-3 sm:grid-cols-2 md:grid-cols-3">
-              {allColors.map((color, index) =>
-                courses.data[index] ? (
-                  <div key={index} {...color} className={`flex flex-col gap-1 p-3 border-2 rounded-xl ${color.bordercolor} md:h-[10.5rem] sm:h-[10rem]`}>
+              {courses.data.map((course, i) => {
+                return (
+                  <div className=" group flex flex-col gap-3 rounded-xl md:h-[10.5rem] sm:h-[11.5rem] h-[9rem] justify-center p-3 shadow-lg border-slate-100 border-1" key={i}>
                     <header className="flex flex-row w-fit ">
-                      <div className={`border-2 rounded-full w-14 h-14 ${color.bgcolor} ${color.bordercolor}`}>
-                        <FaUser className="w-full h-full scale-75 rounded-full" />
+                      <div className="bg-teal-200 border-2 border-teal-800 rounded-full w-14 h-14">
+                        <BsJournalBookmark className="w-full h-full scale-50" />
                       </div>
-                      <div className={`relative w-24 h-5 my-auto text-center rounded-r-full ${color.bgcolor} ${color.bordercolor} right-2 -z-10`}>
-                        <p className="text-xs font-medium">{courses.data[index].ficha}</p>
+                      <div className="relative w-24 h-5 my-auto text-center bg-teal-200 border-2 border-teal-800 rounded-r-full right-2 -z-10">
+                        <p className="text-xs font-medium">{course.ficha}</p>
                       </div>
                     </header>
-                    <p className="text-[13px] font-light text-justify">{courses.data[index].name}</p>
+                    <section>
+                      <p className="text-sm font-medium">{course.name}</p>
+                      <span className="text-xs font-light"></span>
+                    </section>
                     <div className="mt-auto ml-auto w-fit">
-                      <Button value={'Asignar'} rounded="rounded-full" bg="bg-rosa" px="px-3" py="py-[4px]" textSize="text-sm" font="font-medium" clickeame={handleAsign} textColor="text-white" icon={<HiOutlineUserAdd className="text-xl" />} />
+                      <Button value={'Asignar'} rounded="rounded-full" bg="bg-slate-200" px="px-3" py="py-[4px]" textSize="text-sm" font="font-medium" clickeame={handleAsign} textColor="text-slate-600" icon={<HiOutlineUserAdd className="text-xl" />} />
                     </div>
                   </div>
-                ) : null
-              )}
+                )
+              })}
             </section>
           </section>
           <Footer />
