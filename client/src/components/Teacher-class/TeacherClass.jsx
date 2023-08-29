6c3a399ab792008f53916cbafa2561c17a800188
Loren Q <lorenquiceno@gmail.com>
2023-08-29 14:05:46 -0500

    feat: added component to view a teacher's courses

---
+++ client/src/components/Teacher-class/TeacherClass.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Skeleton from 'react-loading-skeleton'
+
+// Components
+import { getClassByTeacherId } from '../../api/httpRequest'
+import { Footer } from '../Footer/Footer'
+import { Search } from '../Search/Search'
+import { Siderbar } from '../Siderbar/Sidebar'
+import { Pagination } from '../Utils/Pagination/Pagination'
+import { Card3D } from '../Utils/Card/Card'
+
+export const TeacherClass = () => {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(true)
+  const [teacherClass, setTeacherClass] = useState([])
+  const [pageNumber, setPageNumber] = useState(0)
+
+  useEffect(() => {
+    getTeacherClass(id)
+  }, [id])
+
+  const getTeacherClass = async (id) => {
+    try {
+      const response = await getClassByTeacherId(id)
+      const { data } = response.data
+      setTeacherClass(data)
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
+  useEffect(() => {
+    setLoading(false)
+  }, [])
+
+  const coursesPerPage = 6
+  const pageCount = Math.ceil(teacherClass.length / coursesPerPage)
+  const startIndex = pageNumber * coursesPerPage
+  const endIndex = startIndex + coursesPerPage
+
+  console.log(teacherClass)
+
+  return (
+    <main className='flex flex-row min-h-screen bg-whitesmoke'>
+      <Siderbar />
+      <section className='relative grid flex-auto w-min grid-rows-3-10-75-15'>
+        <header className='grid place-items-center'>
+          <Search searchFilter />
+        </header>
+        <section className='flex flex-col h-full gap-3'>
+          <section className='grid grid-cols-1 px-10 pt-3 pb-2 gap-x-4 gap-y-7 md:h-[85%] sm:grid-cols-2 md:grid-cols-3'>
+            {loading ? (
+              <>
+                <SkeletonLoading number={6} />
+              </>
+            ) : (
+              teacherClass.slice(startIndex, endIndex).map((course, i) => {
+                return <Card3D key={i} header={course.numero_ficha} title={course.nombre_programa_formacion} subtitle={course.estado} item1={course.seguimiento_nombre_completo} item2={course.lider_nombre_completo} item3={course.fecha_fin_lectiva.split('T')[0]} item4={course.fecha_inicio_practica.split('T')[0]} />
+              })
+            )}
+          </section>
+          <div className='flex justify-center h-[13vh] relative bottom-0'>
+            <Pagination setPageNumber={setPageNumber} pageCount={pageCount} />
+          </div>
+        </section>
+        <Footer />
+      </section>
+    </main>
+  )
+}
+
+const SkeletonLoading = ({ number = 6 }) => [...Array(number)].map((_, i) => <Card3D header={<Skeleton />} title={<Skeleton />} subtitle={<Skeleton />} item1={<Skeleton />} item2={<Skeleton />} item3={<Skeleton />} item4={<Skeleton />} key={i} />)
