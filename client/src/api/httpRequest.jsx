ea109ed7b3e7daa82ae3cc1c9b4eb3d159b4eaab
Loren Q <lorenquiceno@gmail.com>
2023-08-30 11:33:32 -0500

    feat: added getUserById from api

--- client/src/api/httpRequest.jsx
+++ client/src/api/httpRequest.jsx
@@ -160,3 +160,11 @@ export const getAvalById = async (id) => {
   const response = await axios.get(URL)
   return response
 }
+
+// OBTENER USERS BY ID
+export const getUserById = async (id) => {
+  const URL = `${baseUrl}${api}/user/${id}`
+
+  const response = await axios.get(URL)
+  return response
+}
