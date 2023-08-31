d078e59328c1801a1cbc2ff13b6641f340c48e60
ConanGH-S <blandon0207s@gmail.com>
2023-08-31 11:11:12 -0500

    feat: button component new props (alpha)

--- client/src/components/Utils/Button/Button.jsx
+++ client/src/components/Utils/Button/Button.jsx
@@ -1,6 +1,6 @@
-export const Button = ({ value, bg = 'bg-primary', px = 'px-[5rem]', textColor = 'text-white', rounded = 'rounded-md', shadow = 'shadow-none', clickeame, font = 'font-semibold', textSize = 'text-lg', py = 'py-1.5', hover = 'hover-none', icon, isDisabled = false, type = 'button' }) => {
+export const Button = ({ value = '', bg = 'bg-primary', px = 'px-[5rem]', textColor = 'text-white', rounded = 'rounded-md', shadow = 'shadow-none', onClick, font = 'font-semibold', textSize = 'text-lg', py = 'py-1.5', hover = false, icon, isDisabled = false, type = 'submit', hoverConfig = '', classNames }) => {
   return (
-    <button type={type} disabled={isDisabled} className={`${bg} ${rounded} ${px} ${shadow} ${py} mx-auto ${textColor} ${textSize} ${font} ${hover} transition-colors ${icon && 'flex items-center gap-1'}`} onClick={clickeame}>
+    <button type={type} disabled={isDisabled} className={`${isDisabled ? 'bg-slate-500' : bg} ${rounded} ${px} shadow-${shadow} shadow-current ${py} mx-auto ${textColor} ${textSize} ${font} ${hover && `transition-colors hover:${hoverConfig}`} ${icon && 'flex items-center gap-1'} ${classNames}`} onClick={onClick}>
       {icon} {value}
     </button>
   )
