40ab5064f7144e56246e2ad333dd5407943f977a
ConanGH-S <blandon0207s@gmail.com>
2023-10-20 16:17:10 -0500

    docs: added in `documents.routes.ts`

--- server/src/routes/documents.routes.ts
+++ server/src/routes/documents.routes.ts
@@ -5,11 +5,49 @@ import { checkInstructor, checkModality } from '../middlewares/documents.middlew
 
 const documentRoutes: IRouter = Router()
 
+/**
+ * @route GET /create-excel-class
+ * @description Genera un archivo de Excel relacionado con las clases.
+ * @param numero_ficha Número de la clase.
+ * @access Público
+ */
 documentRoutes.get('/create-excel-class', checkClassNumber, excelGeneratorClass)
+
+/**
+ * @route GET /create-excel-students
+ * @description Genera un archivo de Excel de todos los estudiantes.
+ * @access Público
+ */
 documentRoutes.get('/create-excel-students', excelGeneratorStudents)
+
+/**
+ * @route GET /create-excel-students-practical
+ * @description Genera un archivo de Excel relacionado con los estudiantes en prácticas.
+ * @access Público
+ */
 documentRoutes.get('/create-excel-students-practical', excelGeneratorStudentsPractical)
+
+/**
+ * @route GET /create-excel-students-nopractical
+ * @description Genera un archivo de Excel relacionado con los estudiantes sin prácticas.
+ * @access Público
+ */
 documentRoutes.get('/create-excel-students-nopractical', excelGeneratorStudentsNoPractical)
+
+/**
+ * @route GET /create-excel-students-category
+ * @description Genera un archivo de Excel relacionado con los estudiantes por modalidad de practicas.
+ * @param modality Nombre modalidad de practicas.
+ * @access Público
+ */
 documentRoutes.get('/create-excel-students-category', checkModality, excelGeneratorStudentsCategory)
+
+/**
+ * @route GET /create-excel-students-instructor
+ * @description Genera un archivo de Excel relacionado con los estudiantes por instructor.
+ * @param instructor Nombre completo del instructor.
+ * @access Público
+ */
 documentRoutes.get('/create-excel-students-instructor', checkInstructor, excelGeneratorStudentsInstructor)
 
 export { documentRoutes }
