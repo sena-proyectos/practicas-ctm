6aaea18c1a19b3a4aa93bf822102b963fb415c33
Luis Miguel <luismiguealvarezmarin109@gmail.com>
2024-07-18 07:09:05 -0500

    feat: :sparkles: add manual de usuarios

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -11,7 +11,7 @@ config()
 
 const PORT = process.env.PORT ?? 3000
 const DB_USER = process.env.DB_USER ?? 'root'
-const DB_PASSWORD = process.env.DB_PASSWORD ?? '1234'
+const DB_PASSWORD = process.env.DB_PASSWORD ?? '1234SENA'
 const DB_HOST = process.env.DB_HOST ?? 'localhost'
 const DB_DATABASE = process.env.DB_DATABASE ?? 'sena_practicas2'
 const DB_PORT = process.env.DB_PORT ?? 3306
